b47841c93dc03443fc9500791d2fd8eb17e388ff
Marc Lamberti <Marc@Marcs-MacBook-Pro.local>
2014-11-09 03:09:31 +0800

    add wav reader and beginning of spectral centroid feature

--- Makefile
+++ Makefile
@@ -1,6 +1,6 @@
 CC	= g++ -std=c++11
 CFLAGS 	= -c -Wall
-LDFLAGS = -lavcodec -lavformat -lavutil -lz -lm -laubio
+LDFLAGS = -lavcodec -lavformat -lavutil -lz -lm -laubio -lxtract
 SRCS	= $(wildcard src/*.cc)
 OBJS	= $(SRCS:.cc=.o)
 BIN	= MusicEmotionRecognition
--- src/main.cc
+++ src/main.cc
@@ -9,46 +9,93 @@
 #include <map>
 #include <unistd.h>
 #include <vector>
+#include <cstdint>
+#include <array>
+
+#include "xtract/libxtract.h"
+#include "xtract/xtract_scalar.h"
+#include "xtract/xtract_helper.h"
+#include "WaveFile.h"
 
 namespace {
-	std::vector<std::vector<float>> features;
 
-	const int size = 512;
+	const int kBlockSize = 512;
+	const int kNumberOfJazzSong = 85;
+	const int kNumberOfClassicalSong = 65;
+
+	enum Format {
+		MIDI,
+		WAV,
+		NUMBER_OF_FORMAT,
+	};
+
+	std::vector<std::string> kFormatToString = {
+		".mid",
+		".wav",
+	};
+
+	enum Genders {
+		CLASSICAL,
+		JAZZ,
+		NUMBER_OF_GENDERS
+	};
+
+	enum Modes {
+		MINOR,
+		MAJOR,
+		NUMBER_OF_MODES
+	};
+
+	enum Keys {
+		C,
+		DB,
+		D,
+		EB,
+		E,
+		F,
+		GB,
+		G,
+		AB,
+		A,
+		BB,
+		B,
+		NUMBER_OF_KEYS
+	};
+
+	std::map<std::string, enum Keys> kStringToKey = {
+		{"C", Keys::C},
+		{"DB", Keys::DB},
+		{"D", Keys::D},
+		{"EB", Keys::EB},
+		{"E", Keys::E},
+		{"F", Keys::F},
+		{"GB", Keys::GB},
+		{"G", Keys::G},
+		{"AB", Keys::AB},
+		{"A", Keys::A},
+		{"BB", Keys::BB},
+		{"B", Keys::B}
+	};
+
+	int kModeAndKeyIndex = 10;
+	int kTempoIndex = 6;
+
+	enum Features {
+		BPM, // OK
+		AVERAGE_ENERGY,
+		ENERGY_STANDARD_DEVIATION,
+		AVERAGE_FUNDAMENTAL_FREQUENCY,
+		FUNDAMENTAL_FREQUENCY_STANDARD_DEVIATION,
+		NUMBER_OF_FREQUENCIES_HIGHER_THAN_AVEREAGE_FUNDAMENTAL_FREQUENCY,
+		AVERAGE_CENTROID,
+		CENTROID_STANDARD_DEVIATION,
+		MODE, // OK
+		KEY, // OK
+		GENDER, // OK
+		NUMBER_OF_FEATURES
+	};
+
 }
-
-
-/**
- * Read the sound file and display a block of hop_size samples
- * @param  file the soung file
- * @return      true if there was no error, otherwise false
- */
-bool	readWav(const std::string &file) {
-	uint_t 	samplerate = 0;
-  	uint_t 	hop_size = 256;
-  	uint_t 	n_frames = 0;
-  	uint_t	read = 0;
-
-  	aubio_source_t *aubio_source = new_aubio_source(const_cast<char *>(file.c_str()), samplerate, hop_size);
-  	if (!aubio_source) {
-  		std::cerr << "new_aubio_source failed" << std::endl;
-  		return false;
-  	}
-  	
-  	fvec_t *vec = new_fvec(hop_size);
-  	samplerate =  aubio_source_get_samplerate(aubio_source);
-
-  	do {
-  		aubio_source_do(aubio_source, vec, &read);
-  		fvec_print(vec);
-  		n_frames += read;
-  	} while (read == hop_size);
-
-  	std::cout << "read " << n_frames << " frames at " << samplerate << "Hz (" << n_frames / hop_size << " blocks) from " << file << std::endl;
-	del_fvec(vec);
- 	del_aubio_source(aubio_source);
-  	return true;
-}
-
 
 std::string	GetFileExtension(std::string file) {
 	std::string::size_type idx = file.rfind('.');
@@ -56,81 +103,6 @@ std::string	GetFileExtension(std::string file) {
 		return file.substr(idx + 1);;
 	return std::string("");
 }
-
-int kNumberOfJazzSong = 85;
-int kNumberOfClassicalSong = 65;
-
-enum Format {
-	MIDI,
-	WAV,
-	NUMBER_OF_FORMAT,
-};
-
-std::vector<std::string> kFormatToString = {
-	".mid",
-	".wav",
-};
-
-enum Genders {
-	CLASSICAL,
-	JAZZ,
-	NUMBER_OF_GENDERS
-};
-
-enum Modes {
-	MINOR,
-	MAJOR,
-	NUMBER_OF_MODES
-};
-
-enum Keys {
-	C,
-	DB,
-	D,
-	EB,
-	E,
-	F,
-	GB,
-	G,
-	AB,
-	A,
-	BB,
-	B,
-	NUMBER_OF_KEYS
-};
-
-std::map<std::string, enum Keys> kStringToKey = {
-	{"C", Keys::C},
-	{"DB", Keys::DB},
-	{"D", Keys::D},
-	{"EB", Keys::EB},
-	{"E", Keys::E},
-	{"F", Keys::F},
-	{"GB", Keys::GB},
-	{"G", Keys::G},
-	{"AB", Keys::AB},
-	{"A", Keys::A},
-	{"BB", Keys::BB},
-	{"B", Keys::B}
-};
-
-int kModeAndKeyIndex = 10;
-int kTempoIndex = 6;
-
-enum Features {
-	BPM,
-	AVERAGE_ENERGY,
-	ENERGY_STANDARD_DEVIATION,
-	AVERAGE_FUNDAMENTAL_FREQUENCY,
-	FUNDAMENTAL_FREQUENCY_STANDARD_DEVIATION,
-	NUMBER_OF_FREQUENCIES_HIGHER_THAN_AVEREAGE_FUNDAMENTAL_FREQUENCY,
-	AVERAGE_CENTROID,
-	CENTROID_STANDARD_DEVIATION,
-	MODE,
-	KEY,
-	GENDER,
-	NUMBER_OF_FEATURES
-};
 
 void split(const std::string &s, char delim, std::vector<std::string> &elems) {
     std::stringstream ss(s);
@@ -147,8 +119,6 @@ std::vector<std::string> Split(const std::string &s, char delim) {
 }
 
 std::string 	ExecCommand(std::string const &cmd) {
-	int		status;
-	pid_t	pid;
 	std::string output;
 	
 	FILE *lsofFile_p = popen(cmd.c_str(), "r");
@@ -222,15 +192,39 @@ enum Keys ExtractKey(std::vector<std::string> const &midi_info) {
 	return Keys::NUMBER_OF_KEYS;
 }
 
-void	FillFeatures(std::vector<float> &tuple, int song_id, std::string const &song_path) {
-	std::cout << "Name is : " << song_path << std::endl;
-	tuple.reserve(NUMBER_OF_FEATURES);
-	std::vector<std::string> info_in_midi = Split(GetInfoFromMIDI(song_path), ';');
+void	MidiFeatures(std::vector<float> &tuple, int song_id, std::string const &song_midi_path) {
+	std::vector<std::string> info_in_midi = Split(GetInfoFromMIDI(song_midi_path), ';');
 	tuple[BPM] = ExtractBpm(info_in_midi);
 	tuple[MODE] = static_cast<float>(ExtractMode(info_in_midi));
 	tuple[KEY] = static_cast<float>(ExtractKey(info_in_midi));
 	tuple[GENDER] = static_cast<float>(ExtractGender(song_id));
+}
 
+void	ExtractSpectrumCentroids() {
+	std::array<float, 4> argv;
+	float *window;
+
+	window = (float *)xtract_init_window(kBlockSize, XTRACT_HANN);
+}
+
+void	WavFeatures(std::vector<float> &tuple, std::string const &song_wav_path) {
+	WaveFile wav_file(song_wav_path);
+	if (!wav_file.IsLoaded()) {
+		return;
+	}
+	float *data = (float *)wav_file.GetData();
+	std::size_t bytes = wav_file.GetDataSize();
+	std::uint64_t samples = bytes / sizeof(float);
+	std::vector<float> wav_data(samples);
+	std::copy(data, data + samples, wav_data.begin());
+
+}
+
+void	FillFeatures(std::vector<float> &tuple, int song_id, std::string const &song_midi_path, std::string const &song_wav_path) {
+	std::cout << "Name is : " << song_midi_path << std::endl;
+	tuple.reserve(NUMBER_OF_FEATURES);
+	//MidiFeatures(tuple, song_id, song_midi_path);
+	WavFeatures(tuple, song_wav_path);
 	std::cout << "The beat is : " << tuple[BPM] << std::endl;
 	std::cout << "The mode is : " << tuple[MODE] << std::endl;
 	std::cout << "The key is : " << tuple[KEY] << std::endl;
@@ -246,9 +240,10 @@ int main(int ac, char **av){
 	}
 	int song_id = 1;
 	for (auto &tuple : data_set) {
-		std::string song_path = av[1] + std::string("/") + IdToSongName(song_id, Format::MIDI);
+		std::string song_midi_path = av[1] + std::string("/") + IdToSongName(song_id, Format::MIDI);
+		std::string song_wav_path = av[1] + std::string("/") + IdToSongName(song_id, Format::WAV);
 		if (song_id < 9)
-		FillFeatures(tuple, song_id++, song_path);
+			FillFeatures(tuple, song_id++, song_midi_path, song_wav_path);
 	}
     return 0;
 }

fbc05b6a8f900f11c69d4826c4534f7a170a037f
Jordy Domingos <Dyjor@wf102-075.ust.hk>
2014-11-08 20:13:22 +0800

    Clean up the main.cc

--- src/main.cc
+++ src/main.cc
@@ -16,72 +16,6 @@ namespace {
 	const int size = 512;
 }
 
-// std::vector<float>	getBeats(const std::string &file) {
-// 	aubio_tempo_t 		*tempo;
-//   	uint_t 				win_size 		= size;
-//   	uint_t 				hop_size 		= size / 4;
-//   	uint_t 				n_frames 		= 0;
-//   	uint_t				read 			= 0;
-// 	aubio_source_t		*aubio_source 	= NULL;
-// 	uint_t				samplerate		= 0;
-// 	std::vector<float> 	beats;
-
-// 	// Create a new source object from the file given as parameter
-// 	// If samplerate is set to 0, the sample rate of the original file is used
-// 	// hop_size is the size of the blocks to read from the source
-// 	if (!(aubio_source = new_aubio_source (const_cast<char *>(file.c_str()), samplerate, hop_size))) {
-// 		std::cerr << "new_aubio_source failed" << std::endl;
-// 		aubio_cleanup();
-// 		return beats;
-// 	}
-	
-// 	// Get the samplerate from the file
-// 	if ((samplerate = aubio_source_get_samplerate(aubio_source)) == 0) {
-// 		std::cerr << "aubio_source_get_samplerate failed" << std::endl;
-// 		del_aubio_source(aubio_source);
-// 		aubio_cleanup();
-// 		return beats;
-// 	}
-	
-// 	// Create two vectors
-// 	// One input buffer and one for the output position
-// 	fvec_t *in 	= new_fvec(hop_size);
-//   	fvec_t *out = new_fvec(2);
-
-//   	// Create the tempo object
-//   	// The "default" method is used, beattracking, win_size is the length of FFT
-//   	// hop_size is the number of frames between two consecutive runs
-//   	std::string tempo_method = "default";
-// 	if (!(tempo = new_aubio_tempo(const_cast<char *>(tempo_method.c_str()), win_size, hop_size, samplerate))) {
-// 		std::cerr << "new_aubio_tempo failed" << std::endl;
-// 		del_aubio_source(aubio_source);
-// 		aubio_cleanup();
-// 		return beats;		
-// 	}
-
-// 	// Find the tempo
-// 	do {
-// 		// Put data in the input buffer
-// 		aubio_source_do(aubio_source, in, &read);
-// 		// tempo detection
-// 		aubio_tempo_do(tempo, in, out);
-// 		// manipulate the beat
-// 		if (out->data[0] != 0) {
-// 			beats.push_back(aubio_tempo_get_bpm(tempo));
-//       		std::cout << "beat at " << aubio_tempo_get_last_ms(tempo) << "ms, " << aubio_tempo_get_last_s(tempo) << "s, frame " << aubio_tempo_get_last(tempo) << ", " << aubio_tempo_get_bpm(tempo) << "bpm with confidence " << aubio_tempo_get_confidence(tempo) << std::endl;
-//       	}
-//     	n_frames += read;
-//   	} while (read == hop_size);
-
-//   	std::cout << "read " << n_frames * 1. / samplerate << "s, " << n_frames << " frames at " << samplerate << "Hz ("<< n_frames / hop_size << " blocks) from " << file << std::endl;
-
-// 	// clean up memory
-//   	del_aubio_tempo(tempo);
-//   	del_fvec(in);
-//   	del_fvec(out);
-//   	del_aubio_source(aubio_source);
-//   	return beats;
-// }
 
 /**
  * Read the sound file and display a block of hop_size samples
@@ -115,11 +49,6 @@ bool	readWav(const std::string &file) {
   	return true;
 }
 
-// void	getFeatures(const std::string &file) {
-// //	readWav(file);
-// 	getBeats(file);
-// }
-
 
 std::string	GetFileExtension(std::string file) {
 	std::string::size_type idx = file.rfind('.');
@@ -127,40 +56,6 @@ std::string	GetFileExtension(std::string file) {
 		return file.substr(idx + 1);;
 	return std::string("");
 }
-
-// /**
-//  * Normalize the dataset using timidity to convert .midi to .wave
-//  * Each song is converted into wave (32bit float) making it mono with a bit rate of 128kbps
-//  * and a sampling frequency of 44.1kHz
-//  * Ugly implementation
-//  * @param file to process and directory in which we should put the processed sound.
-//  */
-// void	ProcessSoundMidiToWave(std::string const &file, std::string const &directory) {
-// 	std::string extension = GetFileExtension(file);
-// 	if (extension == "mid") {
-// 		std::string filename = file.substr(0, file.length() - (extension.length() + 1));
-// 		std::string cmd = "timidity -Ow \"" + directory + "/" + file + "\"";
-// 		ExecCommand(cmd);
-// 	}
-// }
-
-// bool	NormalizeDataSet(std::string const &directory) {
-// 	DIR		*dir;
-// 	dirent	*pdir;
-
-// 	if ((dir = opendir(directory.c_str())) == NULL){
-// 		std::cerr << "Open directory " << directory << " failed" << std::endl;
-// 		return false;
-// 	}
-
-// 	while ((pdir = readdir(dir))) {
-// 		if (pdir->d_type == DT_REG) {
-// 			ProcessSoundMidiToWave(pdir->d_name, directory);
-// 		}
-// 	}
-// 	closedir(dir);
-// 	return true;
-// }
 
 int kNumberOfJazzSong = 85;
 int kNumberOfClassicalSong = 65;
@@ -335,6 +230,7 @@ void	FillFeatures(std::vector<float> &tuple, int song_id, std::string const &son
 	tuple[MODE] = static_cast<float>(ExtractMode(info_in_midi));
 	tuple[KEY] = static_cast<float>(ExtractKey(info_in_midi));
 	tuple[GENDER] = static_cast<float>(ExtractGender(song_id));
+
 	std::cout << "The beat is : " << tuple[BPM] << std::endl;
 	std::cout << "The mode is : " << tuple[MODE] << std::endl;
 	std::cout << "The key is : " << tuple[KEY] << std::endl;
